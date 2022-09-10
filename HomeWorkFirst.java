06b171c07393e7db6e67cc2bfa2da781784b5b7a
Rinat Dusumov <rinat_dusumow@int.pl>
2022-09-10 18:37:09 +0200

    Update HomeWorkFirst.java

--- HomeWorkFirst.java
+++ HomeWorkFirst.java
@@ -8,7 +8,7 @@
 
 class HomeWorkFirst {
     public static void main(String[] args) {
-        int x, y, P, S;
+        int x, y, p, s;
         x = 8; y = 12;
         P = 2 * (x + y);
         S = x * y;
@@ -16,14 +16,15 @@ class HomeWorkFirst {
 
         int z, k;
         z = 5; k = 3;
-        double h = z / k * 1.0;
+        double h = z * 1.0/ k;
         System.out.printf((z + k) +" "+ (z - k) +" "+ (z * k) +" "+ "%.2f\n", h);
 
         int[] num = {35, 6, 4, 87, 42};
         System.out.println(num[0] +" "+ num[1] +" "+ num[2] +" "+ num[3] +" "+ num[4]);
 
-        int l = 25;
-        double caloricity = (Math.PI * Math.pow(l/2, 2)) * 40;
+        int d = 25;
+        int r = d / 2;
+        double caloricity = (Math.PI * Math.pow(r, 2)) * 40;
         System.out.printf("The calorie content of pizza is equal to %.2f\n", caloricity);
     }
 }
