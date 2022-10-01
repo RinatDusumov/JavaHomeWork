de824e2da0d0d85fb337a372d33b77ba5407c334
Rinat Dusumov <rinat_dusumow@int.pl>
2022-10-01 17:30:49 +0200

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,52 @@
+public class Car {
+    boolean isGasoline;
+    boolean batteryIsCharged;
+    boolean gasPedal;
+    boolean brakePedal;
+    boolean toBrake;
+    boolean ignition;
+    boolean clutch;
+    public Car(boolean isGasoline, boolean batteryIsCharged, boolean gasPedal,
+               boolean brakePedal, boolean toBrake, boolean ignition, boolean clutch) {
+        this.isGasoline = isGasoline;
+        this.batteryIsCharged = batteryIsCharged;
+        this.gasPedal = gasPedal;
+        this.brakePedal = brakePedal;
+        this.toBrake = toBrake;
+        this.ignition = ignition;
+        this.clutch = clutch;
+    }
+    public void clampingTheClutch() {
+        if (ignitionOn()) {
+            clutch = true;
+            System.out.println("Сцепление зажато");
+        }
+    }
+    public boolean ignitionOn() {
+        if (isGasoline && batteryIsCharged) {
+            ignition = true;
+            System.out.println("Включено зажигание");
+            return true;
+        } else {
+            System.out.println("Машина не готова к эксплуатации");
+            return false;
+        }
+    }
+    public boolean giveGas() {
+        gasPedal = true;
+        return true;
+    }
+    public void clutchRelease() {
+        clutch = false;
+        System.out.println("Сцепление отжато");
+    }
+    public boolean braking() {
+        gasPedal = false;
+        brakePedal = true;
+        return true;
+    }
+    public void ignitionOff() {
+        ignition = false;
+        System.out.println("Зажигание выключено");
+    }
+}
