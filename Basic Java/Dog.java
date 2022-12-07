115707268f9a7c3b6f09925e91d5c6415dd5242a
Rinat Dusumov <rinat_dusumow@int.pl>
2022-12-07 10:15:45 +0100

    Create Dog.java

---
+++ Basic Java/Dog.java
@@ -0,0 +1,8 @@
+public class Dog extends Animal implements IAnimal {
+    public Dog(String name, String color, int age) {
+        super(name, color, age);
+    }
+    public String voice() {
+        return "gaf-gaf";
+    }
+}
