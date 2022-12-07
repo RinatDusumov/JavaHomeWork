310dbde66222e6fadfa55ee559aa42a021eaef14
Rinat Dusumov <rinat_dusumow@int.pl>
2022-12-07 10:15:56 +0100

    Create HomeWorkEighthSecondLevel.java

---
+++ Basic Java/HomeWorkEighthSecondLevel.java
@@ -0,0 +1,14 @@
+/**
+ * Java Basic. Home work #8
+ *
+ * @author Rinat Dusumov
+ * @todo 28.09.2022
+ * @date 01.10.2022
+ */
+public class HomeWork {
+    public static void main(String[] args) {
+        Car car = new Car(true, false, false,
+                false, false, false, false);
+        car.machineControl();
+    }
+}
