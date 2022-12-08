451c5d5be9fe7aecb65caa9ddf0785027bd58c61
Rinat Dusumov <rinat_dusumow@int.pl>
2022-12-08 09:33:43 +0100

    Update AdditionalTask_06_12.java

--- PRO Java/AdditionalTask_06_12.java
+++ PRO Java/AdditionalTask_06_12.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
-public class AdditionalTask_06_12 {
-    public static void main(String[] args) {
-        String string = "hello world world";
-        findMostFrequentWord(string);
-        //System.out.println(findMostFrequentWord(string));
+public class AdditionalTask_06_12 {     
+public static void main(String[] args) {
+    String string = "hello world world";
+    findMostFrequentWord(string);
+    //System.out.println(findMostFrequentWord(string));
     }
     public static void findMostFrequentWord(String text) {
         String[] str = text.split(" ");
@@ -12,13 +12,13 @@ public class AdditionalTask_06_12 {
         String newStr = null;
         for (int i = 0; i < str.length; i++) {
             for(int j = 0; j < str.length - 1; j++) {
-                if (str[i].equalsIgnoreCase(str[j++])) {
+                if (str[i].equalsIgnoreCase(str[++j])) {
                     newStr = str[j];
                     count++;
                     break;
                 }
             }
         }
-        System.out.printf(newStr + " %d", count);
+        System.out.printf(newStr + "- %d", count);
     }
 }
