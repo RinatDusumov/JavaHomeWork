a18dc0c5520607c9cd4f96751764c712104f299c
Rinat Dusumov <rinat_dusumow@int.pl>
2022-12-11 19:57:40 +0100

    Create CatThree.java

---
+++ BasicJava/CatThree.java
@@ -0,0 +1,11 @@
+public class CatThree implements Action {
+    @Override
+    public String jog() {
+        return "Cat running";
+    }
+
+    @Override
+    public String bounce() {
+        return "cat jumping";
+    }
+}
